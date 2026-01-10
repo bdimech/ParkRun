641d126e438781c8118c0996f9fbd38910c38ff7
bdimech <brendan.dimech@gmail.com>
2026-01-10 16:20:49 +1030

    Set up organized project structure with Flask dashboard framework

---
+++ dashboard/data_fetcher.py
@@ -0,0 +1,16 @@
+"""
+Module for fetching ParkRun data
+"""
+
+def fetch_parkrun_results(athlete_id):
+    """
+    Fetch ParkRun results for a given athlete ID
+
+    Args:
+        athlete_id: The ParkRun athlete ID
+
+    Returns:
+        dict: Athlete results data
+    """
+    # TODO: Implement ParkRun data fetching
+    pass
